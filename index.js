3997b968586df69c14c0a18e5ca95018b5096b08
Julia123-web <juliadesouzaesilva@gmail.com>
2020-02-05 17:24:37 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+// app.use(express.json());
+
+// app.get("/", () => {});
+
+app.listen(port, () => console.log(`App started in port: ${port}`));
